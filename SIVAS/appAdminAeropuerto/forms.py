6e8eb263a5d78b00dff56ed70f4af817abfe670f
Lexan92 <>
2018-05-24 13:34:12 -0600

    se agrego campos IATA al modelo de PAIS y CIUDAD

--- SIVAS/appAdminAeropuerto/forms.py
+++ SIVAS/appAdminAeropuerto/forms.py
@@ -6,11 +6,14 @@ class AeropuertoForm(forms.ModelForm):
         model = aeropuerto
         exclude = ()
         field = [
-            '',
-            '',
-            '',
-            '',
+            'nombre_aeropuerto',
+            'telefono_aeropuerto',
+            'nombre_responsable',
+            'ciudad',
+            
         ]
         widgets = {
-            
+            'nombre_aeropuerto':forms.TextInput(attrs={'class':'form-control','placeholder':'Introduzca el nombre del Aeropuerto','autofocus':'True','required':'True','maxlenght':'50'}),
+            'telefono_aeropuerto':forms.TextInput(attrs={'class':'form-control','placeholder':'Numero telefonico principal','autofocus':'True','required':'True','maxlength':'15'}),
+            'nombre_responsable':forms.TextInput(attrs={'class':'form-control','placeholder':'Escriba el nombre del responsable','autofocus':'True','required':'True','maxlenght':'50'}),   
         }
